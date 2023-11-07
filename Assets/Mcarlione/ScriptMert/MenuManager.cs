cece508bf7048d804aba7cfc5dbfff52c9e5755f
McarLione53 <mertomeraslan@ogrenci.beykoz.edu.tr>
2023-11-07 21:48:54 +0300

    Menugecis
    menugecis

--- Assets/Mcarlione/ScriptMert/MenuManager.cs
+++ Assets/Mcarlione/ScriptMert/MenuManager.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using TMPro;
+using JetBrains.Annotations;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField] private RectTransform _Title;
     [SerializeField] private RectTransform _Settings;
-    [SerializeField] private int _YValue;
-    [SerializeField] private int _YValueSettings;
-    [SerializeField,Range(0,10)] private float _Lenght=2;
+    [SerializeField] public Vector2 _Duration;
+    [SerializeField] public Vector2 _EntryLocation;
+    [SerializeField] public Vector2 _ExitLocation;
+    [SerializeField] private Animator _ClickToStartAnimation;
     void Start()
     {
-      _Title.DOLocalMove(new Vector2(_Title.position.x,_YValue),_Lenght);
-      _Settings.DOLocalMove(new Vector2(_Title.position.x,_YValueSettings),_Lenght);
+        _Title.DOLocalMove(new Vector2(_Title.position.x,_EntryLocation.x),_Duration.x);
+        _Settings.DOLocalMove(new Vector2(_Title.position.x,_EntryLocation.y),_Duration.x);
     }
+    public void SwitchScene()
+    {
+        _ClickToStartAnimation.SetTrigger("SwitchScene");
+        _Title.DOLocalMove(new Vector2(_Title.position.x,_ExitLocation.x),_Duration.y);
+        _Settings.DOLocalMove(new Vector2(_Title.position.x,_ExitLocation.y),_Duration.y);
+
+    }
+        
+    
+
 }
